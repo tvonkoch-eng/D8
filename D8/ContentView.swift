bdfd76e73c7b558fa15ec7017566b557758e846b
tvonkoch-eng <tobyvonkoch@gmail.com>
2025-09-02 19:05:29 -0700

    Initial Commit

---
+++ D8/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  D8
+//
+//  Created by Tobias  Vonkoch  on 9/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ D8/D8App.swift
@@ -0,0 +1,17 @@
+//
+//  D8App.swift
+//  D8
+//
+//  Created by Tobias  Vonkoch  on 9/2/25.
+//
+
+import SwiftUI
+
+@main
+struct D8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
